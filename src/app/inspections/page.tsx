17a0d48f293e3f184db6860e46e7a2d478d6adb0
Jeferson Gomes <jeferson_gjr@hotmail.com>
2026-02-22 17:08:59 -0300

    feat: create modal to add new inspection

--- src/app/inspections/page.tsx
+++ src/app/inspections/page.tsx
@@ -5,6 +5,7 @@ import { getInspections } from "@/services/inspections.service";
 import styles from "./page.module.css";
 import { Inspection } from "@/types/inspections";
 import Card from "@/app/components/organisms/Card";
+import { ModalOverlay } from "../components/atoms/Modal";
 
 export default function Home() {
   const [inspections, setInspections] = useState<Inspection[]>([]);
@@ -19,6 +20,14 @@ export default function Home() {
             {inspections.map((inspection) => (
         <Card key={inspection.id} inspection={inspection} />
       ))}
+      <ModalOverlay className={styles.modalOverlay}>
+          <form className={styles.form}>
+            <input type="text" placeholder="Location" />
+            <input type="text" placeholder="Inspector" />
+            <input type="text" placeholder="Date" />
+            <input type="text" placeholder="Initial State" />
+          </form>
+      </ModalOverlay>
       </section>
 
     </div>
