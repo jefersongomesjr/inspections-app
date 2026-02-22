19ec84d0d0de9f5823ddd349617d71eb762cb5d3
Jeferson Gomes <jeferson_gjr@hotmail.com>
2026-02-22 17:09:37 -0300

    refactor: rename components

---
+++ src/app/components/organisms/Card/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Inspection } from '@/types/inspections';
+import styles from './Card.module.css';
+
+interface CardProps {
+  inspection: Inspection;
+}
+
+export default function Card({ inspection }: CardProps) {
+  return (
+    <div className={styles.card}>
+      <h2>{inspection.location}</h2>
+      <p><strong>Inspector:</strong> {inspection.inspector}</p>
+      <p><strong>Date:</strong> {inspection.date}</p>
+      <p><strong>Initial State:</strong> {inspection.status ? 'Em Andamento' : 'Concluído'}</p>
+      {inspection.status && <p><strong>Status:</strong> {inspection.status}</p>}
+      <div> 
+        <button>Editar</button>
+        <button>Editar</button>
+      </div>
+    </div>
+  );
+}
