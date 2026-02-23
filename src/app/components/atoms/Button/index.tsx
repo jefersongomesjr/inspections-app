4990592447e2d2164a0e5602121b0f0fd33e270d
Jeferson Gomes <jeferson_gjr@hotmail.com>
2026-02-22 21:17:56 -0300

    fix: fix props name

--- src/app/components/atoms/Button/index.tsx
+++ src/app/components/atoms/Button/index.tsx
@@ -1,16 +1,17 @@
-type ContainerProps = {
+import styles from './Button.module.css'; // Add this import
+
+type Button = {
   textAction: string;
   type?: "button" | "submit" | "reset";
   variant?: "primary" | "secondary";
-  className: string;
   onClick: () => void
 };
 
-export function Button({ textAction, variant, onClick, type}: ContainerProps) {
+export function Button({ textAction, variant, onClick, type}: Button) {
   return (
-    <button 
-        type={type} 
-        className={`btn ${variant}`} 
+    <button
+        type={type}
+        className={`${styles.btn} ${variant && styles[variant]}`} // Update className
         onClick={onClick}>{textAction}
     </button>
   )
